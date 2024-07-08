e4d343bf5e3ce8f367167be4a715e9e4e97b0123
w11wo <wilsonwong961@gmail.com>
2024-07-08 18:00:47 +0700

    Moved Dependencies to setup.py

--- README.md
+++ README.md
@@ -13,7 +13,6 @@ This library grabs multiple information of a Swahili word. Given a word, the lib
 
 ```sh
 git clone https://github.com/bookbot-hive/word_info_sw.git
-pip install -r word_info_sw/requirements.txt
 pip install word_info_sw/
 ```
 
--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import os
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")) as f:
     long_description = f.read()
 
+install_requires = ["gruut[sw]", "openai==0.28"]
+
 if __name__ == "__main__":
     setup(
         name="word_info_sw",
@@ -15,6 +17,8 @@ if __name__ == "__main__":
         url="https://github.com/bookbot-hive/word_info_sw",
         license="Apache License",
         packages=find_packages(),
+        install_requires=install_requires,
+        dependency_links=["https://synesthesiam.github.io/prebuilt-apps/"],
         include_package_data=True,
         platforms=["linux", "unix", "windows"],
         python_requires=">=3.6",
